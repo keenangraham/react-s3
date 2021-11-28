65904b1ec5c4a0624f7c45e731a7aa4fe98d56bd
Keenan Graham <keenangraham@stanford.edu>
2021-11-28 11:45:32 -0800

    Init

---
+++ tests/unit/test_react_s3_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from react_s3.react_s3_stack import ReactS3Stack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in react_s3/react_s3_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = ReactS3Stack(app, "react-s3")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
